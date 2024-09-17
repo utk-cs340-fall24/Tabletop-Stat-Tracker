c5f813be94c68057f4114b1db9f0947be8ba84a5
McKinley Harmon <mcharmon57@gmail.com>
2024-09-16 20:53:20 -0400

    Added more functionality

--- helloplus/mharmo21/player.cpp
+++ helloplus/mharmo21/player.cpp
@@ -4,6 +4,20 @@
 player::player(std::string n, int a){
     name = n;
     health = a;
+    health_cap = a;
+}
+
+std::string player::name_print(){
+    return name;
+}
+
+int player::health_print(){
+    return health;
+}
+
+void player::player_print(){
+    std::cout << "Name:   " << name_print() << '\n'
+              << "Health: " << health_print() << '\n';
 }
 
 
@@ -11,6 +25,10 @@ void player::health_increment(int a){
     if(a <= 0) std::cerr << "Not a valid damage counter";
 
     health += a;
+    if(health > health_cap){
+        std::cout << "Healed past cap.\n";
+        health = health_cap;
+    }
 }
 
 
@@ -19,4 +37,7 @@ void player::health_decrement(int a){
     if(a <= 0) std::cerr << "Not a valid damage counter";
 
     health -= a;
+    if(health < 0){
+        std::cout << "Player is knocked.... begin death saves >:)\n";
+    }
 }
--- helloplus/mharmo21/player.h
+++ helloplus/mharmo21/player.h
@@ -1,16 +1,20 @@
 #include <string>
 #include <iostream>
-
+#include <string.h>
 class player{
 
     private:
     int health;
+    int health_cap;
     std::string name;
 
     public:
     player(std::string, int);
     void health_decrement(int);
     void health_increment(int);
+    int health_print();
+    std::string name_print();
+    void player_print();
 
 
 };
