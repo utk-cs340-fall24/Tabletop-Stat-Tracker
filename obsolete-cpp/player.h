053411ed07fa29765aca45d00c1b845828eed692
Aaron Sparks <aaronsparks2022@gmail.com>
2024-10-11 13:14:32 -0400

    Transition to Node.JS Server

---
+++ obsolete-cpp/player.h
@@ -0,0 +1,47 @@
+#include <string>
+#include <iostream>
+#include <vector>
+
+struct item{
+    std::string name;
+    int count = 0;
+    std::string description;
+};
+
+
+
+class player
+{
+    private:
+        //general information
+        std::string name;
+        
+        //inventory
+        std::vector<item> inventory;
+
+        //health information
+        int health;
+        int health_cap;
+        int death_save_fails;
+        int death_save_sucesses;
+
+    public:
+        //general
+        player(std::string, int);
+        void player_print();
+        std::string name_print();
+        
+        //inventory
+        void inventory_print();
+        void inventory_insert(std::string);
+        void inventory_remove(std::string);
+    
+        //health
+        void health_decrement(int);
+        void health_increment(int);
+        int  health_print();
+        //! might need to delete later
+        void death_fail();
+        void death_success();
+        //! --------------------------
+};
