3fa5aa3eec67acd438bb14fafe5406db88e15053
aaronsparks2022@gmail.com <aaronsparks2022@gmail.com>
2024-10-13 19:02:34 -0400

    Node.js Server Additions

--- RetroRoller/server.js
+++ RetroRoller/server.js
@@ -1,12 +1,26 @@
-var WebSocketServer = require('websocket').server;
-
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
 console.log("Starting Server...")
+function makeid(length) {
+    let result = '';
+    const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        counter += 1;
+    }
+    return result;
+}
+
+let host_token = makeid(5);
+console.log("Host Token: " + host_token);
+let player_token = makeid(5);
+console.log("Player Token: " + player_token);
 var server = http.createServer(function (req, res) {
     var q = url.parse(req.url, true);
-    var filename = "./resources/web" + q.pathname;
+    var filename = "./web" + q.pathname;
     fs.readFile(filename, function (err, data) {
         if (err) {
             res.writeHead(404, { 'Content-Type': 'text/html' });
